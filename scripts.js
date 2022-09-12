b1a89de0cae7c763df5b07097f03b68fc5311720
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-09-12 14:20:17 -0400

    updated handle function with suffix variable and set property functionality

--- scripts.js
+++ scripts.js
@@ -3,7 +3,13 @@ const inputs = document.querySelectorAll(".controls input");
 
 // function to handle updates of controls
 function handleUpdate() {
-    console.log(this.value);
+
+    const suffix = this.dataset.sizing || "";
+
+    // using the custom variable we created, and making this variable using this.name (note each input is given a corresponding name to target that input), which will variably set the value to what was selected
+        // when we created the suffix variable, we can add that to the end which will add "px" to the end of the variable value allowing this to be adjusted accordingly (without we see that the value is changing but this is not readable code by the computer)
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+    
 }
 
 // attaching a change event listener that will call the handleupdate function
