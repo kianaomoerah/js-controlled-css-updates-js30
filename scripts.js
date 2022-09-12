c0e0e4b510b0d15f251fc294f6cf9a0946fc7b6e
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-09-12 14:04:54 -0400

    base function and event listeners for input controls

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-// scripts
+// target each target
+const inputs = document.querySelectorAll(".controls input");
+
+// function to handle updates of controls
+function handleUpdate() {
+    console.log(this.value);
+}
+
+// attaching a change event listener that will call the handleupdate function
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+
+// attached a change event listener for when the mouse moves not just when the click is released
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
+
